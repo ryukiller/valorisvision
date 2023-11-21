242fd862deb878b5a270f11d04731ee6bbd0476f
ryukiller <rian.rodrigo.webmaster@gmail.com>
2023-11-21 10:29:38 +0100

    first commit

--- README.md
+++ README.md
@@ -1,23 +1,20 @@
-ValorisVisio: A Crypto Market Visualization Tool
-================================================
+# ValorisVisio: A Crypto Market Visualization Tool
 
 Welcome to the GitHub repository for ValorisVisio, the innovative cryptocurrency comparison tool that bridges the gap between individual holdings and global market movements. Derived from Latin, our name combines "valor" (value) and "visio" (vision), reflecting our commitment to providing insightful, value-driven analytics in the crypto space.
 
-About ValorisVisio
-------------------
+## About ValorisVisio
 
 ValorisVisio is a dynamic app designed to empower crypto investors and enthusiasts. It allows users to compare their cryptocurrency holdings with overall market capitalizations, helping them visualize potential gains and understand their investments in the context of broader market trends.
 
 ### Key Features:
 
--   Market Comparison: Easily compare your crypto portfolio against various market caps.
--   Potential Gain Visualization: See potential gains with intuitive, interactive charts and graphs.
--   Real-Time Data Analysis: Access up-to-date market data for accurate comparisons and analysis.
--   User-Friendly Interface: Navigate through a clean, straightforward user experience.
--   Diverse Cryptocurrency Support: Includes a wide range of cryptocurrencies for comprehensive portfolio management.
+- Market Comparison: Easily compare your crypto portfolio against various market caps.
+- Potential Gain Visualization: See potential gains with intuitive, interactive charts and graphs.
+- Real-Time Data Analysis: Access up-to-date market data for accurate comparisons and analysis.
+- User-Friendly Interface: Navigate through a clean, straightforward user experience.
+- Diverse Cryptocurrency Support: Includes a wide range of cryptocurrencies for comprehensive portfolio management.
 
-Getting Started
----------------
+## Getting Started
 
 To get started with ValorisVisio:
 
@@ -25,17 +22,14 @@ To get started with ValorisVisio:
 2.  Install Dependencies: Navigate to the project directory and run `npm install` (assuming Node.js is installed).
 3.  Run the Application: Execute `npm start` to launch ValorisVisio on your local machine.
 
-Contributing
-------------
+## Contributing
 
 We welcome contributions to ValorisVisio! If you have suggestions or improvements, please fork the repository and create a pull request, or open an issue with the tag "enhancement". Don't forget to give the project a star if you find it useful!
 
-License
--------
+## License
 
 ValorisVisio is released under the MIT License. Feel free to use, modify, and distribute it as per the license terms.
 
-Contact
--------
+## Contact
 
 For any queries or support, please reach out to us at <riangrapphics@gmail.com>.
---
+++ src/app/page.js
@@ -0,0 +1,45 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      <h1>ValorisVisio Crypto Scenarios, what if...</h1>
+      <h2>Ever wonder what your bag value will be if your crypto holdings market cap matches another crypto project?</h2>
+      <p>Here you can visualize your potential gains in that possible scenario, just input your holdings and select your target crypto project, and visualize how mutch your current holdings value would be in that case.</p>
+
+
+      <h2>Introduction to ValorisVisio Crypro Scenarios</h2>
+      <p>In the ever-evolving landscape of cryptocurrency, investors and enthusiasts are constantly seeking tools that not only simplify their investment decisions but also provide a deeper understanding of market dynamics. ValorisVisio, drawing its name from the Latin words for value ("valoris") and vision ("visio"), epitomizes the fusion of insightful market analysis with user-centric design, offering a unique perspective on cryptocurrency investments. Our mission is to empower users with a tool that not only tracks and analyzes their crypto holdings but also contextualizes them within the broader market, offering a comprehensive view of potential scenarios and outcomes.</p>
+      <h3>The Essence of ValorisVisio</h3>
+      <p>ValorisVisio is more than just a cryptocurrency tracking app. It is a sophisticated platform designed to cater to both novice and experienced crypto investors. The app provides an immersive experience into the world of cryptocurrency, enabling users to explore various 'crypto scenarios' - potential market movements and their impacts on individual portfolios.</p>
+      <h3>Key Functionalities</h3>
+      <h4>Interactive Crypto Scenario Modeling</h4>
+      <p>At the heart of ValorisVisio is our innovative 'Crypto Scenario Modeling' feature. This tool allows users to create and analyze hypothetical market situations, helping them understand how different market conditions could affect their portfolio. Users can adjust variables like market cap, coin value, and global economic factors to see how these changes might influence their investments.</p>
+      <h4>Real-Time Market Data and Analysis</h4>
+      <p>ValorisVisio integrates real-time market data, ensuring that users have the latest information at their fingertips. This feature is crucial for making informed decisions, particularly in a market as volatile as cryptocurrency. The app not only tracks prices but also offers in-depth analysis of market trends, helping users identify opportunities and risks.</p>
+      <h4>Portfolio Tracking and Performance Metrics</h4>
+      <p>Users can easily track their crypto holdings, with detailed performance metrics that go beyond mere price tracking. ValorisVisio provides insights into the overall health of your portfolio, including asset diversification, risk assessment, and long-term performance trends.</p>
+      <h4>Educational Resources and Insights</h4>
+      <p>Recognizing the importance of informed decision-making, ValorisVisio offers a wealth of educational resources. These include market analyses, investment strategy guides, and scenario-based learning modules, all designed to enhance users' understanding of crypto scenarios and investment strategies.</p>
+      <h4>Customizable Alerts and Notifications</h4>
+      <p>Stay ahead of the market with customizable alerts. Users can set notifications for price changes, market news, or scenario outcomes, ensuring they never miss an important market movement or update.</p>
+      <h4>Community and Social Features</h4>
+      <p>ValorisVisio fosters a community of crypto enthusiasts and experts. Users can share scenarios, strategies, and insights, creating a collaborative environment for learning and growth.</p>
+      <h4>Secure and Private</h4>
+      <p>Security and privacy are paramount in the crypto world. ValorisVisio employs state-of-the-art security measures to protect user data, ensuring peace of mind for all our users.</p>
+      <h3>Bridging the Gap: Individual Holdings and Market Dynamics</h3>
+      <p>One of the most significant aspects of ValorisVisio is its ability to bridge the gap between individual crypto holdings and broader market dynamics. By offering a clear visualization of how market cap fluctuations can impact personal investments, the app provides users with a holistic view of their investment strategy's effectiveness.</p>
+      <h3>Scenario-Based Planning and Strategy</h3>
+      <p>ValorisVisio's scenario-based planning tools are a cornerstone of its functionality. Users can explore 'what-if' situations, allowing them to prepare for various market conditions. This feature is invaluable for risk management and strategic planning, offering a sandbox environment to test different investment approaches.</p>
+      <h3>User Experience: Intuitive and Engaging</h3>
+      <p>The user experience in ValorisVisio is designed to be both intuitive and engaging. The interface is clean and easy to navigate, making it accessible for users of all levels of crypto expertise. Interactive charts, graphs, and visual aids are used extensively to make complex data understandable and actionable.</p>
+      <h3>Advanced Analytics for Informed Decisions</h3>
+      <p>Our advanced analytics tools dissect vast amounts of market data to provide users with precise, actionable insights. Whether it's identifying emerging trends or understanding the impact of global economic changes on the crypto market, ValorisVisio equips users with the knowledge they need to make informed decisions.</p>
+      <h3>Personalized Experience</h3>
+      <p>ValorisVisio understands that every investor&rsquo;s needs are unique. The app allows users to customize their experience, from the types of alerts they receive to the way data is displayed, ensuring that every interaction with the app is tailored to individual preferences and investment styles.</p>
+      <h3>Conclusion</h3>
+      <p>ValorisVisio is more than just an app; it's a comprehensive platform designed for anyone interested in cryptocurrency. By focusing on crypto scenarios and providing a suite of tools for analysis, tracking, and education, ValorisVisio stands as a beacon of clarity and insight in the often turbulent seas of the crypto world. Join us on this journey, and take control of your crypto investment future with ValorisVisio.</p>
+    </main>
+  )
+}
