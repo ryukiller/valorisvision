0e7f2d84920d25a093f0a22ac5057c75875b352a
ryukiller <rian.rodrigo.webmaster@gmail.com>
2023-11-21 10:54:32 +0100

    added analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 
+import Script from 'next/script'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,6 +13,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-ETPN827MV5" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-ETPN827MV5');
+        `}
+      </Script>
       <body className={inter.className}>{children}</body>
     </html>
   )
