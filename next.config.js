8c17ea66843f57c93153c86fd46e5d26df18dec7
ryukiller <rian.rodrigo.webmaster@gmail.com>
2024-09-09 16:21:50 +0200

    blog

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        domains: ['valorisvisio.top', 'coin-images.coingecko.com', 'assets.coingecko.com'],
         remotePatterns: [
             {
                 protocol: 'https',
